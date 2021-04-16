8d95e24145bd926a4b83119838cebeb55f9ca0c3
Jaden783 <65601364+Jaden783@users.noreply.github.com>
2021-04-16 19:13:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBpFaTYnrsRPC4Kv8tiGHkN1y2yXDuWQjk",
+  authDomain: "barter-app-ec1b4.firebaseapp.com",
+  projectId: "barter-app-ec1b4",
+  storageBucket: "barter-app-ec1b4.appspot.com",
+  messagingSenderId: "474503556152",
+  appId: "1:474503556152:web:6e070a406a1f6ccd3d0acd",
+  measurementId: "G-P31SPN8HRL"
+};
